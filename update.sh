3e4fee2b7e633c1672f14255ba8b6ab9b48fdea2
Kenji Saito <ken-yo@mbr.nifty.com>
2025-02-25 22:32:58 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -2,37 +2,38 @@
 
 CUR=$(pwd)
 
-CURRENT=$(cd $(dirname $0);pwd)
+CURRENT=$(cd "$(dirname "$0")" || exit;pwd)
 echo "${CURRENT}"
 
-cd "${CURRENT}/app"
+cd "${CURRENT}/app" || exit
 git pull --prune
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
+echo ""
 pwd
-npx corepack use pnpm@latest && pnpm install && pnpm up && pnpm build
+npx corepack use pnpm@latest && pnpm install && pnpm up -r && pnpm build
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
-cd "${CURRENT}"
+cd "${CURRENT}" || exit
 git pull --prune
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
+echo ""
 pwd
-
 git commit -am "Bumps node modules" && git push
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 
-cd "${CUR}"
+cd "${CUR}" || exit
